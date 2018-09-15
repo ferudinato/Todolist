9ce3b5f37bee24779d9d898ed14fb94d1c4ce6f3
FernanityChamp <andres.fernando08@gmail.com>
2018-09-15 11:35:28 -0500

    aplicado los estilos en los componentes que muestra el listado de tareas y en el input

--- src/components/NewTodo/NewTodo.js
+++ src/components/NewTodo/NewTodo.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
+import Stylesheet from './NewTodo.css';
+
 const newTodo = (props) => {
     return (
-        <div>
-            <label>
-                Tarea:
-                <input type='text' />
-            </label>
-            <button>Agregar</button>
+        <div className={Stylesheet.NewTodo}>
+            <form onSubmit={props.submitted}>
+                <label>
+                    <span>Tarea:</span>
+                    <input type='text' name='taskName' autoComplete='off' />
+                </label>
+                <button type='submit'>Agregar</button>
+            </form>
         </div>
     )
 }
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
+import Stylesheet from './TodoList.css';
+
+
 const todoList = (props) => {
+    let tasks = props.taskList.map((item, i) => {
+        return (<li key={item.id}>
+            <div>{item.name}</div>
+            <div className={Stylesheet.CloseBtn}><button onClick={() => props.clicked(item.id)}>X</button></div>
+        </li>)
+    })
+
     return (
-        <div>
-            Todolist here
+        <div className={Stylesheet.TodoList}>
+            {
+                tasks.length > 0
+                ?
+                    (
+                        <ul>
+                            {tasks}
+                        </ul>
+                    )
+                : <p className={Stylesheet.NoTaskYet}>No se han agregado tareas aún.</p>
+            }
         </div>
     )
 }
