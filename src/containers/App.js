2be4d1ce8fad4270521802efc35de5385f842985
FernanityChamp <andres.fernando08@gmail.com>
2018-09-15 09:52:10 -0500

    utilizando los componentes dentro del componente principal

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 
 import Stylesheet from './App.css';
 
+import TodoList from '../components/TodoList/TodoList';
+import NewTodo from '../components/NewTodo/NewTodo';
+
+
 class App extends Component {
   render() {
     return (
       <div className={Stylesheet.App}>
         <h2>Organizalo Ya!</h2>
+        <TodoList />
+        <NewTodo />
       </div>
     );
   }
