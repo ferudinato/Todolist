96b0cefbeb90c840f5bed156e85f63a7abc60853
FernanityChamp <andres.fernando08@gmail.com>
2018-09-15 09:50:43 -0500

    configurado entorno inicial

---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Stylesheet from './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={Stylesheet.App}>
+        <h2>Organizalo Ya!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
